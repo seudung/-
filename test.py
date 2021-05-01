9f0fcc9f174f54a45d4598ec28ada67b37dbec1f
seudung <83484677+seudung@users.noreply.github.com>
2021-05-01 11:59:24 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pyupbit
 
-access = "oIEH7JUxtFDPsgYqdSxAjKymQad5Dau3mr81yrrA"          # 본인 값으로 변경
-secret = "2op4XxzEyt4uWUj0A2JsDyzvslvK8h9vyyJVNJrQ"          # 본인 값으로 변경
+access = "oo"          # 본인 값으로 변경
+secret = "oo"          # 본인 값으로 변경
 upbit = pyupbit.Upbit(access, secret)
 
 print(upbit.get_balance("KRW-QKC"))     # KRW-QKC 조회
